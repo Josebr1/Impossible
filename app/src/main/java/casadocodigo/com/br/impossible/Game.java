9e6c3fb11faf3f80d91e4392a5f1e30d850248a4
José <jose.silva.br@outlook.com>
2016-10-31 23:43:12 -0200

    Detectando toques na tela.

--- app/src/main/java/casadocodigo/com/br/impossible/Game.java
+++ app/src/main/java/casadocodigo/com/br/impossible/Game.java
@@ -3,11 +3,13 @@ package casadocodigo.com.br.impossible;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Game extends AppCompatActivity {
+public class Game extends AppCompatActivity implements View.OnTouchListener{
 
     private Impossible view;
 
@@ -20,6 +22,9 @@ public class Game extends AppCompatActivity {
         // Configura view
         setContentView(view);
 
+        // Configura o toque na tela
+        view.setOnTouchListener(this);
+
        /* /* Usar a tela cheia do aparelho
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(
@@ -31,5 +36,11 @@ public class Game extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         view.resume();
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        view.moveDown(10);
+        return true;
     }
 }
