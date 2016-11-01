9ffa571c76b7ca131e3328c515d7f3095d0d7e0f
José <jose.silva.br@outlook.com>
2016-10-31 22:48:48 -0200

    Tela cheia do aparelho.

--- app/src/main/java/casadocodigo/com/br/impossible/Game.java
+++ app/src/main/java/casadocodigo/com/br/impossible/Game.java
@@ -2,6 +2,8 @@ package casadocodigo.com.br.impossible;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Game extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        /* Usar a tela cheia do aparelho */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
